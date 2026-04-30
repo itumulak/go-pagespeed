b36d6b5711dac7aa37c355f170e761043fb7f510
Ian Tumulak <edden87@gmail.com>
2026-04-30 21:25:32 +0800

    initialize

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	urls := []string{
+		"https://avocadova.com",
+		"https://staging.avocadova.com",
+		"https://tmtinsurance.com",
+		"https://www.allyhealthgroup.net",
+	}
+}
